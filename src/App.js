dfeb37f51fa9bab3450ff0fe481b57d556ac39c3
artcheeze <surasak.jaikum@mail.kmutt.ac.th>
2018-06-22 03:39:37 +0700

    align detail and connect DB

--- src/App.js
+++ src/App.js
@@ -1,56 +1,89 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-import Home from './home'
-import Auth from './Auth'
-import './App.css'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Home from "./home";
+import Auth from "./Auth";
+import "./App.css";
 import { Animated } from "react-animated-css";
 
 
-
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       user: false,
       name: "",
       pass: "",
       show: false
-    }
-    this.isLogin = this.isLogin.bind(this)
-    this.isLogout = this.isLogout.bind(this)
+    };
+    this.isLogin = this.isLogin.bind(this);
+    this.isLogout = this.isLogout.bind(this);
   }
 
   isLogin() {
-    if(this.state.name === this.state.pass){
+    if ((this.state.name === "art") & (this.state.name === this.state.pass)) {
       this.setState({
         user: true
-  
-      })
-    }else{
+      });
+    } else if (
+      (this.state.name === "mon") &
+      (this.state.name === this.state.pass)
+    ) {
+      this.setState({
+        user: true
+      });
+    } else if (
+      (this.state.name === "bank") &
+      (this.state.name === this.state.pass)
+    ) {
+      this.setState({
+        user: true
+      });
+    } else if (
+      (this.state.name === "noey") &
+      (this.state.name === this.state.pass)
+    ) {
+      this.setState({
+        user: true
+      });
+    } else {
       this.setState({
         show: true
-  
-      })
+      });
     }
-    
   }
   isLoginKey(e) {
-    if(e.key === 'Enter'){
-      if(this.state.name === this.state.pass){
+    if (e.key === "Enter") {
+      if ((this.state.name === "art") & (this.state.name === this.state.pass)) {
         this.setState({
           user: true
-    
-        })
-      }else{
+        });
+      } else if (
+        (this.state.name === "mon") &
+        (this.state.name === this.state.pass)
+      ) {
+        this.setState({
+          user: true
+        });
+      } else if (
+        (this.state.name === "bank") &
+        (this.state.name === this.state.pass)
+      ) {
+        this.setState({
+          user: true
+        });
+      } else if (
+        (this.state.name === "noey") &
+        (this.state.name === this.state.pass)
+      ) {
+        this.setState({
+          user: true
+        });
+      } else {
         this.setState({
           show: true
-    
-        })
+        });
       }
     }
-    
-    
   }
 
   isLogout() {
@@ -58,164 +91,150 @@ class App extends Component {
       user: false,
       name: "",
       pass: ""
-    })
-
+    });
   }
 
   render() {
-
-
-
     return (
       <div className="my-app">
-
-
-
-
-
-
         <div className="container-fullhd ">
-
           <div className="notification">
-
-
-
-            <Animated animationIn="tada" animationOut="fadeOut" isVisible={true}>
+            <Animated
+              animationIn="tada"
+              animationOut="fadeOut"
+              isVisible={true}
+            >
               <div>
                 <div class="columns">
-
-
-
                   <div class="column is-three-fifths">
                     <h1 class="title">
-                      <img src="https://image.flaticon.com/icons/svg/138/138292.svg" style={{ width: '50px' }} alt="Coin"/>
+                      <img
+                        src="https://image.flaticon.com/icons/svg/138/138292.svg"
+                        style={{ width: "50px" }}
+                        alt="Coin"
+                      />
                       <strong>หนี้สิน @2302</strong>
                     </h1>
                     <h2 class="subtitle">
                       <code>#ยืมแล้วใช้กันด้วยนะจ้ะ</code>
                     </h2>
-
-
                   </div>
 
+                  <div className="column is-1 " />
 
-
-                  <div className="column is-1 ">
-
-
-
-
-                  </div>
-
-
-
-
-
-                
                   <div class="column is-1" id="A">
-
-                    {this.state.user ? 
-                    [
-                    
-                      <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}> 
-                    <strong>{"Welcome " + this.state.name+ "! "}</strong> 
-                    </Animated>
-                    
-                    ] 
-                    : 
-                    [
-
-                       
-                    <strong><Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>Welcome Guest!  </Animated></strong>
-                   
-
-                    ] 
-                  
-                  }
-
-
-
-                  </div> 
-
-
-
-
-
-
-
+                    {this.state.user
+                      ? [
+                          <Animated
+                            animationIn="flipInX"
+                            animationOut="fadeOut"
+                            isVisible={true}
+                          >
+                            <strong>
+                              {"Welcome " + this.state.name + "! "}
+                            </strong>
+                          </Animated>
+                        ]
+                      : [
+                          <strong>
+                            <Animated
+                              animationIn="flipInX"
+                              animationOut="fadeOut"
+                              isVisible={true}
+                            >
+                              Welcome Guest!{" "}
+                            </Animated>
+                          </strong>
+                        ]}
+                  </div>
 
                   <div class="column is-2 is-gapless ">
-                    {this.state.show ?
-                    [
-                      <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>
-                      <div id="BB" class="column has-text-centered ">
-                      
-                    <div class="notification is-danger">
-                    <button class="delete" onClick={(e) => this.setState({ show: false})}></button>
-                      ชื่อ / รหัส ผิดน้าา ลองใหม่นะจ้ะ :)
-                    </div>
-                     
-                    </div>
-                    </Animated>
-                    
-                    ]
-                       : 
-                       
-                       [
-                        
-                        this.state.user ?
-                        <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>
-                        <div class="column has-text-centered ">
-                        
-                          <button id="B"class="button is-dark is-rounded" onClick={this.isLogout} >{this.state.user ? "Logout" : "Login"}</button>
-                          
-                          </div>
-                          </Animated> 
-                          : 
-                          
-                          <div class="column has-text-centered ">
-                        <Animated animationIn="flipInX" animationOut="fadeOut" isVisible={true}>
-                            <input class="input is-primary is-rounded  is-small" type="text" placeholder="Username" onChange={(e) => this.setState({ name: e.target.value })} />
+                    {this.state.show
+                      ? [
+                          <Animated
+                            animationIn="flipInX"
+                            animationOut="fadeOut"
+                            isVisible={true}
+                          >
+                            <div id="BB" class="column has-text-centered ">
+                              <div class="notification is-danger">
+                                <button
+                                  class="delete"
+                                  onClick={e => this.setState({ show: false })}
+                                />
+                                ชื่อ / รหัส ผิดน้าา ลองใหม่นะจ้ะ :)
+                              </div>
+                            </div>
+                          </Animated>
+                        ]
+                      : [
+                          this.state.user ? (
+                            <Animated
+                              animationIn="flipInX"
+                              animationOut="flipInX"
+                              isVisible={true}
+                            >
+                              <div class="column has-text-centered ">
+                                <button
+                                  id="B"
+                                  class="button is-dark is-rounded"
+                                  onClick={this.isLogout}
+                                >
+                                  {this.state.user ? "Logout" : "Login"}
+                                </button>
+                              </div>
+                            </Animated>
+                          ) : (
+                            <div class="column has-text-centered ">
+                              <Animated
+                                animationIn="flipInX"
+                                animationOut="flipInX"
+                                isVisible={true}
+                              >
+                                <input
+                                  class="input is-primary is-rounded  is-small"
+                                  type="text"
+                                  placeholder="Username"
+                                  onChange={e =>
+                                    this.setState({ name: e.target.value })
+                                  }
+                                />
 
-                            <input class="input is-primary is-rounded  is-small" type="password" placeholder="Password" onChange={(e) => this.setState({ pass: e.target.value })} onKeyPress={(e) => this.isLoginKey(e)}/>
+                                <input
+                                  class="input is-primary is-rounded  is-small"
+                                  type="password"
+                                  placeholder="Password"
+                                  onChange={e =>
+                                    this.setState({ pass: e.target.value })
+                                  }
+                                  onKeyPress={e => this.isLoginKey(e)}
+                                />
 
-                            <a class="button is-primary is-rounded is-pulled-center " onClick={this.isLogin}>Login</a>
-                            </Animated> 
-                          </div>
-
-                      ]
-
-                    }
+                                <a
+                                  class="button is-primary is-rounded is-pulled-center "
+                                  onClick={this.isLogin}
+                                >
+                                  Login
+                                </a>
+                              </Animated>
+                            </div>
+                          )
+                        ]}
                   </div>
-
-
-
                 </div>
               </div>
             </Animated>
-
-
-
-
-
-
-
           </div>
-
         </div>
         <div className="App container">
-          {
-            this.state.user ? 
-            [ 
-            
-            <Route path="/" component={ () => <Home name={this.state.name}/> } />
-          
-            ] 
-            : [ <Route path="/" component={Auth} />] 
-          }
-
-
-
+          {this.state.user
+            ? [
+                <Route
+                  path="/"
+                  component={() => <Home name={this.state.name} />}
+                />
+              ]
+            : [<Route path="/" component={Auth} />]}
         </div>
       </div>
     );
--- src/home.js
+++ src/home.js
@@ -1,49 +1,210 @@
-import React, { Component } from 'react';
-import './home.css'
+import React, { Component } from "react";
+import "./home.css";
 import { Animated } from "react-animated-css";
+import firebase from "firebase";
+
 
 class home extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            username: ""
-        }
+    this.state = {
+      text: "",
+      Bn: "",
+      Nn: "",
+      Mn: "",
+      An: "",
+      bank: [],
+      noey: [],
+      art: [],
+      mon: [],
+      sumB: "ยังไม่เสร็จโว้ยยย",
+      sumM: "ยังไม่เสร็จโว้ยยย",
+      sumN: "ยังไม่เสร็จโว้ยยย",
+      sumA: "ยังไม่เสร็จโว้ยยย"
+    };
+    var config = {
+      apiKey: "AIzaSyCPZtFdctQrB-SyR0sFfYWBW3CTpiqbDi4",
+      authDomain: "finapp-1c327.firebaseapp.com",
+      databaseURL: "https://finapp-1c327.firebaseio.com",
+      projectId: "finapp-1c327",
+      storageBucket: "finapp-1c327.appspot.com",
+      messagingSenderId: "72060197826"
+    };
 
+    if (!firebase.apps.length) {
+      let user = this.props.name;
+      this.app = firebase.initializeApp(config);
+      this.db = this.app.database().ref(user);
+      this.db.on("value", snap => {
+        this.setState({
+          bank: this.state.bank.concat(snap.val().bank),
+          noey: this.state.noey.concat(snap.val().noey),
+          art: this.state.art.concat(snap.val().art),
+          mon: this.state.mon.concat(snap.val().mon),
+          Bn: snap.child(user + "/bank").key,
+          Mn: snap.child(user + "/mon").key,
+          Nn: snap.child(user + "/noey").key,
+          An: snap.child(user + "/art").key
+        });
+        console.log(this.state.sum);
+
+        let app = firebase.app();
+        app.delete(app);
+      });
     }
+  }
 
+  render() {
+    return (
+      <Animated
+        animationIn="bounceInUp"
+        animationOut="fadeOut"
+        isVisible={true}
+      >
+        <div className="tile">
+          <section class="hero is-primary">
+            <div class="hero-body">
+              <div class="container">
+                <h1 class="title">สวัสดีคุณ {this.props.name}</h1>
+                <h2 class="subtitle">#ขอให้ทวงหนี้แล้วได้คืนนะคะ</h2>
 
-
-    render() {
-
-        return (
-            <Animated animationIn="bounceInUp" animationOut="fadeOut" isVisible={true}>
-                <div className="tile">
-                   
-                   <section class="hero is-primary">
-                        <div class="hero-body">
-                            <div class="container">
-                                <h1 class="title">สวัสดีคุณ {this.props.name}</h1>
-                                <h2 class="subtitle">#ขอให้ทวงหนี้แล้วได้คืนนะคะ</h2>
-                                    <div id="data">
-                                     <h2 class="title is-3 is-pulled-left"><li>Title 1</li></h2>
-                                    
-                                    
-                                    
-                                    </div>
+                <div id="data">
+                <Animated
+        animationIn="bounceInUp"
+        animationOut="fadeOut"
+        isVisible={true}
+      >
+                  {this.state.Bn === this.props.name ? (
+                    ""
+                  ) : (
+                    
+                      <section class="section">
+                        <div class="container">
+                          <div>
                                
-                            </div>
+                  <h1 class="title">คุณลูกหนี้ -> {this.state.Bn}</h1>
+                            <h2 class="subtitle">
+                              {this.state.bank.map((index, val) => {
+                                 return <p key={val}>{index}</p>;
+                              })}
+                              <p>Total {this.state.sumB}</p>
+                            </h2>
+                            
+                          </div>
                         </div>
+                      </section>
+                   
+                  )}
+
+
+                  {this.state.Mn === this.props.name ? (
+                    ""
+                  ) : (
+                    
+                      <section class="section">
+                        <div class="container">
+                          <div>
+                            <h1 class="title">คุณลูกหนี้ -> {this.state.Mn}</h1>
+                            <h2 class="subtitle">
+                              {this.state.mon.map(function(index, val) {
+                                 return <p key={val}>{index}</p>;
+                              })}
+                              <p>Total {this.state.sumM}</p>
+                            </h2>
+                          </div>
+                        </div>
+                      </section>
+                   
+                  )}
+
+
+
+                  {this.state.Nn === this.props.name ? (
+                          ""
+                        ) : (
+                
+                    <section class="section">
+                      <div class="container">
+                         
+                          <div>
+                            <h1 class="title">คุณลูกหนี้ -> {this.state.Nn}</h1>
+                            <h2 class="subtitle">
+                              {this.state.noey.map(function(index, val) {
+                                return <p key={val}>{index}</p>;
+                              })}
+                              <p>Total {this.state.sumN}</p>
+                            </h2>
+                          </div>
+                       
+                      </div>
                     </section>
+                  
+                    )}
 
+                      {this.state.An === this.props.name ? (
+                          ""
+                        ) : (
+                 
+                    <section class="section">
+                      <div class="container">
+                      
+                          <div>
+                            <h1 class="title">คุณลูกหนี้ -> {this.state.An}</h1>
+                            <h2 class="subtitle">
+                              {this.state.art.map(function(index, val) {
+                                return <p key={val}>{index}</p>;
+                              })}
+                              <p>Total {this.state.sumA}</p>
+                            </h2>
+                          </div>
+                       
+                      </div>
+                    </section>
+                  
+                   )}
 
-
+                   </Animated>
                 </div>
 
+                
 
-            </Animated>
-        );
-    }
+                {/* {this.state.Mn === this.props.name ? 
+                                     " "
+                                     : <h3 class="subtitle is-4 is-pulled-left">
+                                     <h2 class="title is-3 is-pulled-left">{this.state.Mn}</h2>
+                                     {
+                                        this.state.mon.map(function(index,val){return <li key={val} >{index}</li>})   
+                                     }
+                                     </h3>
+                                     } */}
+
+                {/* {this.state.An === this.props.name ? 
+                                     " "
+                                     : <h3 class="subtitle is-4 is-pulled-left">
+                                     <h2 class="title is-3 is-pulled-left">{this.state.An}</h2>
+                                     {
+                                        this.state.art.map(function(index,val){return <li key={val} >{index}</li>})   
+                                     }
+                                     </h3>
+                                     } */}
+
+                {/* {this.state.Nn === this.props.name ? 
+                                     " "
+                                     : <h3 class="subtitle is-4 is-pulled-left">
+                                     <h2 class="title is-3 is-pulled-left">{this.state.Nn}</h2>
+                                     {
+                                        this.state.noey.map(function(index,val){return <li key={val} >{index}</li>})   
+                                     }
+                                     </h3>
+                                     } */}
+              </div>
+            </div>
+          </section>
+        </div>
+      </Animated>
+    );
+  }
 }
 
 export default home;
